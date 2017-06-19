d82261c12c0afad6476668ee90921bd0abb7f15b
alexandre <alex22trouy5@gmail.com>
2017-06-19 15:53:10 +0200

    premier pas avec ruby

---
+++ Fizz Buzz.rb
@@ -0,0 +1,22 @@
+(1...101).each do |n|
+
+  if n % 3 == 0
+
+
+
+    puts "Fizz"
+
+  elsif n % 5 == 0
+
+
+
+    puts "Buzz"
+
+  else
+
+    
+    puts "#{n}"
+
+  end
+
+end
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # semaine-14
+Premier pas avec ruby.
+
+Exercice Fizz Buzz
